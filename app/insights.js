cc787dcb38a3296f55cf61cd30636408836d2b04
John Watson <john.watson1@defra.gov.uk>
2024-08-12 21:12:27 +0100

    Add cloud role

--- app/insights.js
+++ app/insights.js
@@ -4,6 +4,9 @@ const setupAppInsights = () => {
   if (process.env.APP_INSIGHTS_CONNECTION_STRING) {
     appInsights.setup(process.env.APP_INSIGHTS_CONNECTION_STRING)
     appInsights.start()
+    const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
+    const appName = 'pd-int-registration'
+    appInsights.defaultClient.context.tags[cloudRoleTag] = appName
   }
 }
 
